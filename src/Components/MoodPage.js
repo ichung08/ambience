d922acc87aa7ee5b30579ae497dd100817eea7d0
ChrisFong604 <ccffoonngg@gmail.com>
2021-09-18 01:15:56 -0700

    init commit

--- src/Components/MoodPage.js
+++ src/Components/MoodPage.js
@@ -4,10 +4,7 @@ function MoodPage({ Mood }) {
 	return (
 		<div className="main-container-mood">
 			<h1 className="header-mood">ambience</h1>
-			<p className="subtitle">
-				Take a selfie using your webcam below, and we'll match your mood with a
-				song &#128513;
-			</p>
+			<p className="subtitle">We've detected that you are {Mood}</p>
 		</div>
 	);
 }
