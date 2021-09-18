370e93ea36ded633cb3446ccc03774517bf8dd5e
Isaac Chung <ichung08@students.cs.ubc.ca>
2021-09-17 20:47:20 -0700

    set up components and styles

---
+++ src/Components/Theme.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const FACE_EXPRESSION_LABELS = ["neutral", "happy", "sad", "angry", "fearful", "disgusted", "surprised"];
+
+const Theme = ({ Mood }) => {
+  return <div></div>;
+};
+
+export default Theme;
