8742d31f7e6b38e78120113ae2c63b0f995cc56f
Isaac Chung <ichung08@students.cs.ubc.ca>
2021-09-19 03:57:33 -0700

    updated styles

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,28 +3,10 @@ import { useSpring, animated, config } from "react-spring";
 import "../Styles/index.css";
 
 function Header() {
-  const styles = useSpring({
-    loop: true,
-    to: [
-      { x: 0, opacity: 1, color: "#003ead" },
-      { x: 0, opacity: 1, color: "#1a84cd" },
-      { x: 0, opacity: 1, color: "#00d4ff" },
-      { x: 0, opacity: 1, color: "#8bffd1" },
-      { x: 0, opacity: 1, color: "#00d4ff" },
-      { x: 0, opacity: 1, color: "#1a84cd" },
-      { x: 0, opacity: 1, color: "#003ead" },
-      { x: 0, opacity: 1, color: "#000000" },
-    ],
-    from: { x: 0, opacity: 1, color: "#000000" },
-    config: config.molasses,
-  });
-
   return (
     <>
       <header className="header">
-        <animated.div style={styles}>
-          <h1>ambience</h1>
-        </animated.div>
+        <h1>ambience</h1>
       </header>
     </>
   );
